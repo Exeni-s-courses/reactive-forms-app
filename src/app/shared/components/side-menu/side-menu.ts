1a6f2f6c50f45b74f478cc0162bc712d982ddee5
Javier Exeni <jvexeni@gmail.com>
2025-08-14 18:34:24 -0400

    first commit

---
+++ src/app/shared/components/side-menu/side-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [],
+  templateUrl: './side-menu.html',
+  styles: ``
+})
+export class SideMenu {
+
+}
