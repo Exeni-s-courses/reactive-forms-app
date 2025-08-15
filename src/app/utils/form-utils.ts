9c8cce4480d924badebbad7f4164a9189342b045
Javier Exeni <jvexeni@gmail.com>
2025-08-15 11:54:58 -0400

    refactor dynamic and utils

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,14 +1,7 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
-  static isValidField(form: FormGroup, fieldName: string): boolean | null {
-    return (!!form.controls[fieldName].errors && form.controls[fieldName].touched);
-  }
-
-
-  static getFieldError(form: FormGroup, fieldName: string): string | null {
-    if (!form.controls[fieldName]) return null;
-    const errors = form.controls[fieldName]['errors'] ?? {};
+  static getTextError(errors: ValidationErrors): string | null {
     for (const key of Object.keys(errors)) {
       switch (key) {
         case 'required':
@@ -20,5 +13,32 @@ export class FormUtils {
       }
     }
     return null;
+  }
+
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return (
+      !!form.controls[fieldName].errors && form.controls[fieldName].touched
+    );
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+    const errors = form.controls[fieldName]['errors'] ?? {};
+    return FormUtils.getTextError(errors);
+  }
+
+  static getFieldErrorInArray(
+    formArray: FormArray,
+    index: number
+  ): string | null {
+    if (formArray.controls.length === 0) return null;
+    const errors = formArray.controls[index]['errors'] ?? {};
+    return FormUtils.getTextError(errors);
   }
 }
