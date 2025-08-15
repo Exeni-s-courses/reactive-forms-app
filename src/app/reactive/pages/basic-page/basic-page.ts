62ba1cf6d1a73760f0420f4596e0cc48b5f1ea20
Javier Exeni <jvexeni@gmail.com>
2025-08-14 23:00:21 -0400

    add basic form group

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,12 +1,19 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule,],
   templateUrl: './basic-page.html',
   styles: ``
 })
 export class BasicPage {
+
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  });
 
 }
