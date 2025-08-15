a5510812cd3326f36ed0f090ab6007b9cd42264c
Javier Exeni <jvexeni@gmail.com>
2025-08-14 23:15:50 -0400

    add errors to form group

--- src/app/reactive/pages/basic-page/basic-page.ts
+++ src/app/reactive/pages/basic-page/basic-page.ts
@@ -1,19 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe, ReactiveFormsModule,],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPage {
+  private fb = inject(FormBuilder);
 
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
+  // myForm = new FormGroup({
+  //   name: new FormControl('', [], []),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
 }
