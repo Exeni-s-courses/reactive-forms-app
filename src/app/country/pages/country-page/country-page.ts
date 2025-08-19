acd84b6f1360d3bed127542a85ea0b2004f8f8c2
Javier Exeni <jvexeni@gmail.com>
2025-08-19 16:15:57 -0400

    add country form first select

--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.services';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.html',
   styles: ``
 })
 export class CountryPage {
+  private fb = inject(FormBuilder);
+  countryService = inject(CountryService);
 
+  regions = signal(this.countryService.regions);
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  })
 }
