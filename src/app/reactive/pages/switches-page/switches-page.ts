0d5428996e36352ac63cda4888cb98dc2005d163
David Gemio <davidgemio98@gmail.com>
2025-08-19 11:12:30 -0400

    basic swithces

--- src/app/reactive/pages/switches-page/switches-page.ts
+++ src/app/reactive/pages/switches-page/switches-page.ts
@@ -1,12 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.html',
   styles: ``
 })
 export class SwitchesPage {
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termAndConditions: [true, Validators.requiredTrue],    
+  });
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 }
